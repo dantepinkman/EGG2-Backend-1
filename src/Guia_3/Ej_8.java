55a599ebd988f3066cb6e854835951888c5d6879
CasaMonica <dante.pinkman@gmail.com>
2023-04-08 19:11:53 -0300

    Ej 8 Guia3

---
+++ src/Guia_3/Ej_8.java
@@ -0,0 +1,28 @@
+
+package Guia_3;
+
+import java.util.Scanner;
+
+/**
+ * Dibujar un cuadrado de N elementos por lado utilizando el 
+ * carácter “*”. Por ejemplo, si el cuadrado tiene 
+ * 4 elementos por lado se deberá dibujar lo siguiente:
+  * * * *
+  *     *
+  *     *
+  * * * *
+ */
+public class Ej_8 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        
+        System.out.println("Ingrese tamaño");
+        int N = leer.nextInt();
+        
+        do {            
+            N--;
+            
+        } while (true);
+                
+    }
+}
